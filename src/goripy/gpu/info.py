3715a0d1d09664c13ea869387a519d98ae1646de
RedReservoir <gerard.ortega88@gmail.com>
2026-01-26 11:34:34 +0000

    Updated gpu.info to be indepentend of device name and ID

--- src/goripy/gpu/info.py
+++ src/goripy/gpu/info.py
@@ -4,7 +4,7 @@ import goripy.memory.info
 
 
 
-def sprint_device_info(
+def sprint_device_memory_usage(
     device
 ):
     """
@@ -22,17 +22,14 @@ def sprint_device_info(
             A string with the device information.
     """
 
-    device_id = torch.cuda._get_nvml_device_index(device)
-    device_name = torch.cuda.get_device_name(device)
-
     free_mem_bytes, total_mem_bytes = torch.cuda.mem_get_info(device)
     used_mem_bytes = total_mem_bytes - free_mem_bytes
     
     used_mem_str = goripy.memory.info.sprint_fancy_num_bytes(used_mem_bytes, unit="GiB")
     total_mem_str = goripy.memory.info.sprint_fancy_num_bytes(total_mem_bytes, unit="GiB")
 
-    gpu_log_msg = "Device ID {:d} ({:s}) - Allocated memory: [{:s}] / [{:s}]".format(
-        device_id, device_name, used_mem_str, total_mem_str
+    device_msg = "Allocated memory: [{:s}] / [{:s}]".format(
+        used_mem_str, total_mem_str
     )
 
-    return gpu_log_msg
+    return device_msg
